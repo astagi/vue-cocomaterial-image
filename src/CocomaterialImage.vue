81768a770838ca5e7fa31fc5ae8cd22d7ad42129
Andrea Stagi <stagi.andrea@gmail.com>
2020-08-11 13:31:13 +0200

    First work on performances

--- src/CocomaterialImage.vue
+++ src/CocomaterialImage.vue
@@ -33,31 +33,38 @@ export default {
         .then((data) => {
           const doc = new DOMParser().parseFromString(data.svg_content, 'image/svg+xml');
           this.viewBox = doc.firstElementChild.getAttribute('viewBox')
-          this.paths = Array.from(doc.getElementsByTagName('path')).map(path => {
-            return Array.from(path.attributes).reduce((obj, attr) => {
-              return {...obj, [attr.name] : attr.value }
-            }, {})
-          })
+          this.paths = doc.getElementsByTagName('path')
         }).catch( error => { console.log(error); });
     }
   },
   render: function (createElement) {
-    return createElement(
-      'svg',
-      {
-        attrs: {
-          viewBox: this.viewBox,
-          xmlns: "http://www.w3.org/2000/svg"
-        }
-      }, this.paths.map((path, index) => {
-        return createElement(
-          'path',
-          {
-            attrs: {
-              ...path, fill: index == 0 && this.paths.length > 1 ? this.background : this.foreground
-            }
-          })
-      }))
+    if (this.paths.length > 1) {
+      const pathElements = []
+      for (let i = 0 ; i < this.paths.length ; i++) {
+        pathElements[i] = createElement(
+            'path',
+            {
+              attrs: {
+                d: this.paths[i].attributes.d == undefined ? 'none' : this.paths[i].attributes.d.value,
+                transform: this.paths[i].attributes.transform  == undefined ? 'translate(0, 0)' : this.paths[i].attributes.transform.value,
+                color: this.paths[i].attributes.color  == undefined ? 'none' : this.paths[i].attributes.color.value,
+                overflow: this.paths[i].attributes.overflow  == undefined ? 'none' : this.paths[i].attributes.overflow.value,
+                'paint-order': this.paths[i].attributes['paint-order']  == undefined ? 'none' : this.paths[i].attributes['paint-order'].value,
+                fill: i == 0 && this.paths.length > 1 ? this.background : this.foreground
+              }
+            })
+      }
+      return createElement(
+        'svg',
+        {
+          attrs: {
+            viewBox: this.viewBox,
+            xmlns: "http://www.w3.org/2000/svg"
+          }
+        }, pathElements)
+    } else {
+      return createElement('svg')
+    }
   }
 }
 </script>
